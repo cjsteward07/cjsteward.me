47e8fed6b94915eed50e54d9e8076ada4355065f
cjsteward07 <cjsteward07@gmail.com>
2024-01-18 18:44:43 -0500

    init

---
+++ Cjsteward_Api/DAL/ResumeContext.cs
@@ -0,0 +1,36 @@
+﻿using Cjsteward_Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cjsteward_Api.DAL
+{
+    public class ResumeContext : DbContext
+    {
+        public ResumeContext()
+        {
+
+        }
+        
+        public ResumeContext(DbContextOptions<ResumeContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Bio> Bios { get; set; }
+
+        private static IConfigurationRoot _configuration;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var builder = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+                _configuration = builder.Build();
+                string cnstr = _configuration.GetConnectionString("Cjsteward_Db");
+                optionsBuilder.UseSqlServer(cnstr);
+            }
+        }
+    }
+}
---
+++ Cjsteward_Api/Models/Bio.cs
@@ -0,0 +1,8 @@
+﻿namespace Cjsteward_Api.Models
+{
+    public class Bio
+    {
+        public int Id { get; set; }
+        public byte[]? MyPicture { get; set; }
+    }
+}
